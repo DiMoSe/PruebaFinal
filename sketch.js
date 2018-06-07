1d2bd2098a71d7308407121c70d01da4a770338f
DiMoSe <diegomoralescgu@gmail.com>
2018-06-07 18:44:19 -0500

    audioDone

--- sketch.js
+++ sketch.js
@@ -1,21 +1,16 @@
-/*
- * Cinema Expandido Web
- * Proyecto Final (07/junio/2018)
- * Diego Morales Servin
- * 
- * URL
- */
+//Variables Videos
+var videoInicial;
+var videoCancion;
+var song;
+
+var songPlay = false;
+var videoPlay = true;
+var gameStart = false;
 
 /*
- *****************************************
- * VARIABLES
- *****************************************
- */
-//Variables Videos
-var video;
-var video2;
-var playVideo1 = false;
-var playVideo2 = false;
+//Variables Song
+
+var playSong = false;
 
 //Variables Circle
 var circle1PosX = 500;
@@ -27,57 +22,89 @@ var circle2PosY = 300;
 var diameter = 50;
 var angle = 0;
 var smallDiameter = 60;
+*/
 
 function preload() {
-  video = createVideo("assets/video1.mp4");
-  video2 = createVideo("video2.mp4");
+  videoInicial = createVideo("assets/video1.mp4");
+  videoCancion = createVideo("assets/video2.mp4");
+  song = loadSound("assets/song.mp3");
 }
 
 function setup() {
   //Setup Video
   createCanvas(1920, 720);
-  video.loop();
-  video.hide();
-  video2.loop();
-  video2.hide();
-  
+  imageMode(CENTER);
+  videoInicial.loop();
+  videoInicial.hide();
+  videoCancion.loop();
+  videoCancion.hide();
+  song.loop();
+
+  /*
   //Setup Circle
   noFill();
   strokeWeight(5);
   stroke(255, 204, 0);
+
+  //Setup Music
+*/
 }
 
 function draw() {
   background(0);
-  if (playVideo1 === true) {
-    image(video2, 0, 0);
+
+  if (videoPlay) {
+    image(videoInicial, width / 2, height / 2);
+    song.setVolume(0);
   }
 
-  if (playVideo2 === true) {
-    image(video, 0, 0);
+  if (gameStart) {
+    image(videoCancion, width / 2, height / 2);
+    if (!songPlay) {
+    song.jump(0);
+    songPlay = true;
+    }
+    song.setVolume(1);
+    
   }
-
-  var d1 = smallDiameter + (sin(angle) * diameter / 2) + diameter / 2;
-  ellipse(circle1PosX, circle1PosY, d1, d1);
-  ellipse(circle2PosX,circle2PosY,d1,d1);
-  angle += 0.05;
   
-  print("VIDEO " + playVideo1);
-  print("VIDEO2 " + playVideo2);
+  print (gameStart);
 
+  /*
+  var d1 = smallDiameter + (sin(angle) * diameter / 2) + diameter / 2;
+  if (videoInicial === true) {
+    image(video2, 0, 0);
+    ellipse(circle1PosX, circle1PosY, d1, d1);
+  } else {
+    songPlay();
+    if (playVideo2 === true) {
+      image(video, 0, 0);
+    }
+
+
+
+    ellipse(circle2PosX, circle2PosY, d1, d1);
+    angle += 0.05;
+  }
+  print("VIDEO " + videoInicial);
+  print("VIDEO2 " + playVideo2);
+  */
 }
 
 function mouseClicked() {
-  if (mouseX > (circle1PosX-diameter/2) && mouseX < (circle1PosX+diameter/2) && mouseY > (circle1PosY-diameter/2) && mouseY < (circle1PosY+diameter/2)) {
-    Play1();
-    window.alert("Hello world!");
+  videPlay = false;
+  gameStart = true;
+  /*if (mouseX > (circle1PosX - diameter / 2) && mouseX < (circle1PosX + diameter / 2) && mouseY > (circle1PosY - diameter / 2) && mouseY < (circle1PosY + diameter / 2)) {
+    videoInicial = false;
   }
 
-  if (mouseX > (circle2PosX-diameter/2) && mouseX < (circle2PosX+diameter/2) && mouseY > (circle2PosY-diameter/2) && mouseY < (circle2PosY+diameter/2)) {
+  if (mouseX > (circle2PosX - diameter / 2) && mouseX < (circle2PosX + diameter / 2) && mouseY > (circle2PosY - diameter / 2) && mouseY < (circle2PosY + diameter / 2)) {
     Play2();
   }
+  */
 }
 
+/*
 function Play1() {
   playVideo1 = true;
   playVideo2 = false;
@@ -87,3 +114,4 @@ function Play2() {
   playVideo2 = true;
   playVideo1 = false;
 }
+*/
